0c809c652060f2fa90baeedf350f9cf5be8d3c5b
clj4348 <1136284348@qq.com>
2018-11-12 21:18:42 +0800

    webpack解决css单独分离，js公用模块进行拆分

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 const WebpackMerge = require('webpack-merge');
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin') // 压缩css
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 单独分离打包css
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 清除文件
 const webpack = require('webpack');
 const isDev = process.env.NODE_ENV === 'development'
 function resolve (dir) {
@@ -14,7 +15,7 @@ const config = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].js'
+    filename: 'js/[name].js'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
@@ -33,27 +34,33 @@ const config = {
         test: /\.vue$/,
         loader: 'vue-loader',
       },
+      // .jsx文件处理
       {
         test: /\.jsx$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      // js文件处理
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      // css文件处理
       {
         test: /\.css$/,
         use: [
           'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
         ]
       },
+      // stylus文件处理
       {
         test: /\.styl(us)?$/,
         use: [
           'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'stylus-loader'
         ],
@@ -97,6 +104,26 @@ const config = {
         }
     }
   },
+  optimization:{
+    // minimize: true,
+    runtimeChunk:{
+      name:'manifest'
+    },
+    splitChunks: {
+      chunks: 'async', //默认只作用于异步模块，为`all`时对所有模块生效,`initial`对同步模块有效
+      minSize: 30000, // 合并模块文件的体积
+      minChunks: 1, //最少被引用次数
+      cacheGroups: {
+        vendors: {
+          name: 'vendor',
+          chunks: 'initial',
+          priority: -10,
+          reuseExistingChunk: false,
+          test: /node_modules\/(.*)\.js/
+        }
+      }
+    }
+  },
   plugins:[
     new HtmlWebpackPlugin({
       filename: 'index.html',
@@ -104,10 +131,12 @@ const config = {
       inject: true
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: "css/[name].css",
     }),
     new VueLoaderPlugin()
   ]
+}
+if(!isDev){
+  config.plugins.push(new CleanWebpackPlugin(['dist']))
 }
 module.exports = config;
