df8a46acb8757b25b7587760ad1cefce46559e02
clj4348 <1136284348@qq.com>
2019-08-25 21:54:52 +0800

    0825

--- src/pages/list/components/list-sort.vue
+++ src/pages/list/components/list-sort.vue
@@ -1,5 +1,5 @@
 <template>
-	<ul class="sort-con clearfix">
+  <ul class="sort-con clearfix">
     <li class="sort-item" :class="{'active': act === 0}" @click="actAn(0)">默认排序</li>
     <li class="sort-item" :class="{'active': act === 1}" @click="actAn(1)">
       <span>价格</span>
@@ -9,58 +9,58 @@
   </ul>
 </template>
 <script>
-	export default{
-		name: 'ListSort',
-		data () {
-			return {
-				act: 0,
-				actBefore: ''
-			}
-		},
-		methods:{
-			actAn(option) {
-				if(option == 1){
-					this.act = option
-					this.actBefore = this.actBefore == 1 ? 2 : 1; 
-				}else {
-					this.actBefore = ''
-					this.act = option
-				}
-				this.$emit('search-list',{default:this.act,price:this.actBefore})
-			}
-		}
-	}
+  export default{
+    name: 'ListSort',
+    data () {
+      return {
+        act: 0,
+        actBefore: ''
+      }
+    },
+    methods:{
+      actAn(option) {
+        if(option == 1){
+          this.act = option
+          this.actBefore = this.actBefore == 1 ? 2 : 1; 
+        }else {
+          this.actBefore = ''
+          this.act = option
+        }
+        this.$emit('search-list',{default:this.act,price:this.actBefore})
+      }
+    }
+  }
 </script>
 <style lang="stylus" scoped>
-	.srot-con
-		overflow: hidden
- 		.sort-item
-			float: left
-			display: inline-block
-			position: relative
-			padding: 0 8px
-			width: 60px
-			height: 24px
-			line-height: 24px
-			border: 1px solid #ccc
-			margin-right: -1px
-			background: #fff
-			columns: #333
-			cursor: pointer
-	 		&.active
-				background: #c60023
-				border: 1px solid #c60023
-				color: #fff
-		  .fa-sort-asc
-				position: absolute
-				top: -4px
-				right: 4px
-			.fa-sort-desc
-				position: absolute
-				top: 4px
-				right: 4px
-			&.asc .fa-sort-asc
-				opacity: .6
-	 		&.desc .fa-sort-desc
-				opacity: .6
+  .srot-con
+    overflow: hidden
+    .sort-item
+      float: left
+      display: inline-block
+      position: relative
+      padding: 0 8px
+      width: 60px
+      height: 24px
+      line-height: 24px
+      border: 1px solid #ccc
+      margin-right: -1px
+      background: #fff
+      columns: #333
+      cursor: pointer
+      &.active
+        background: #c60023
+        border: 1px solid #c60023
+        color: #fff
+      .fa-sort-asc
+        position: absolute
+        top: -4px
+        right: 4px
+      .fa-sort-desc
+        position: absolute
+        top: 4px
+        right: 4px
+      &.asc .fa-sort-asc
+        opacity: .6
+      &.desc .fa-sort-desc
+        opacity: .6
 </style>
